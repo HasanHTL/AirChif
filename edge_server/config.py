cab534de76db95ce6228969a71450e3c05e5214f
Hasan Demirci <200149@studierende.htl-donaustadt.at>
2025-09-29 14:34:33 +0200

    created airchif_flutter
    Frontend Project

--- edge_server/config.py
+++ edge_server/config.py
@@ -6,7 +6,7 @@ class Settings:
     SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecret")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 1 day
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
-
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
 settings = Settings()
+
